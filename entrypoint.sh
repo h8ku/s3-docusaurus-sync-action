08bd821390d4f9d8d4c00b0a9ac65a98db58a0e3
H8 <94951726+h8ku@users.noreply.github.com>
2023-12-30 21:25:57 -0800

    Update entrypoint.sh disable yarn

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,15 +31,11 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-
-echo "Install yarn"
-npm install -g yarn
-
 echo "Install dependencies"
-yarn install
+npm install
 
 echo "Run yarn build"
-yarn run build
+npm run build
 
 echo "Copying to website folder"
 aws s3 sync ./build s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
